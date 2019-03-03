f755a2961a381d0fd89583b968c02cd562ad029a
Will McAuliff <womcauliff@gmail.com>
2019-03-03 16:25:26 -0500

    formatting

--- Common-Elements-In-Two-Sorted-Arrays/solution.java
+++ Common-Elements-In-Two-Sorted-Arrays/solution.java
@@ -3,20 +3,20 @@ import java.util.Arrays;
 
 public class CE {
     public static void main(String[] args) {
-        int[] array1A = {1, 3, 4, 6, 7, 9};
-        int[] array2A = {1, 2, 4, 5, 9, 10};
+        int[] array1A = { 1, 3, 4, 6, 7, 9 };
+        int[] array2A = { 1, 2, 4, 5, 9, 10 };
         // commonElements(array1A, array2A) should return [1, 4, 9] (an array).
         Integer[] resultA = CE.commonElements(array1A, array2A);
         System.out.println(Arrays.toString(resultA));
 
-        int[] array1B = {1, 2, 9, 10, 11, 12};
-        int[] array2B = {0, 1, 2, 3, 4, 5, 8, 9, 10, 12, 14, 15};
+        int[] array1B = { 1, 2, 9, 10, 11, 12 };
+        int[] array2B = { 0, 1, 2, 3, 4, 5, 8, 9, 10, 12, 14, 15 };
         // commonElements(array1B, array2B) should return [1, 2, 9, 10, 12] (an array).
         Integer[] resultB = CE.commonElements(array1B, array2B);
         System.out.println(Arrays.toString(resultB));
 
-        int[] array1C = {0, 1, 2, 3, 4, 5};
-        int[] array2C = {6, 7, 8, 9, 10, 11};
+        int[] array1C = { 0, 1, 2, 3, 4, 5 };
+        int[] array2C = { 6, 7, 8, 9, 10, 11 };
         // common_elements(array1C, array2C) should return [] (an empty array).
         Integer[] resultC = CE.commonElements(array1C, array2C);
         System.out.println(Arrays.toString(resultC));
@@ -25,8 +25,8 @@ public class CE {
     /**
      * @param array1 The first array
      * @param array2 The second array
-     * @return The array of Integers that the two arrays have in common.
-     * If none are found, returns an empty ArrayList.
+     * @return The array of Integers that the two arrays have in common. If none are
+     *         found, returns an empty ArrayList.
      */
     public static Integer[] commonElements(int[] array1, int[] array2) {
         Integer[] resultInArray = new Integer[0];
@@ -35,10 +35,7 @@ public class CE {
         int index1 = 0; // index-pointer for array1
         int index2 = 0; // index-pointer for array2
         // Start the search at the lesser of the two first elements.
-        int currentValue = CE.returnLesser(
-            array1[0],
-            array2[0]
-        );
+        int currentValue = CE.returnLesser(array1[0], array2[0]);
 
         // Loop through the two arrays.
         // Stop the search when one of the index-pointer variables
@@ -50,7 +47,7 @@ public class CE {
 
             // Determine if this is a value in common
             if (array1[index1] == array2[index2]) {
-                result.add(new Integer(currentValue));
+                result.add(Integer(currentValue));
             }
 
             // Each time an array DOES contain the current value we are looking for,
@@ -64,17 +61,16 @@ public class CE {
         }
 
         resultInArray = new Integer[result.size()];
-        for(int i = 0; i < result.size(); i++) {   
+        for (int i = 0; i < result.size(); i++) {
             resultInArray[i] = result.get(i);
-        }  
+        }
         return resultInArray;
     }
 
     /**
      * @param num1 The first number
      * @param num2 The second number
-     * @return The lesser of the two numbers, or num1 if
-     * the two numbers are equal.
+     * @return The lesser of the two numbers, or num1 if the two numbers are equal.
      */
     private static int returnLesser(int num1, int num2) {
         return num1 - num2 < 0 ? num1 : num2;
