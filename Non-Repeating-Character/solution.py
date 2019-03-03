a7bdeafcdc07a1ddb6c23a7e7ac271d46366a4e8
Will McAuliff <womcauliff@gmail.com>
2019-03-03 16:54:28 -0500

    adds py solution to non-rep char

---
+++ Non-Repeating-Character/solution.py
@@ -0,0 +1,31 @@
+# Returns the first instance of a non-repeating character in a given string,
+# or None if no non-repeating character exists.
+def non_repeating(given_string):
+    charFreq = {}
+
+    for char in given_string:
+        # Increment by one if the character was seen previously.
+        # A never-before-seen character will start at 1.
+        charFreq[char] = charFreq.get(char, 0) + 1
+
+    # Sweep through string again, and return the first character
+    # that has a total frequency of 1
+    for char in given_string:
+        if (charFreq.get(char) == 1):
+            return char
+
+    # Return None if there are no non-repeating characters
+    return None
+
+
+# should return 'c'
+print(non_repeating("abcab"))
+
+# should return None
+print(non_repeating("abab"))
+
+# should return 'c'
+print(non_repeating("aabbbc"))
+
+# should return 'd'
+print(non_repeating("aabbdbc"))
