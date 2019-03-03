2b4607b5c933c0e5cbffda836a6fc5588bb5d251
Will McAuliff <womcauliff@gmail.com>
2019-03-02 21:55:30 -0500

    Adds missing python solution

--- Is-One-Array-a-Rotation-of-Another/solution.py
+++ Is-One-Array-a-Rotation-of-Another/solution.py
@@ -1,50 +1,59 @@
-# Returns the lesser of the two numbers, or num1 if the two numbers are equal.
-def returnLesser(num1, num2):
-    minimum = num1
-    if num1 - num2 >= 0:
-        minimum = num2
-    return minimum
 
-# Returns the array of integers that the two arrays have in common.
-# If none are found, returns an empty array.
-def common_elements(list1, list2):
-    result = []
-    # Start the search at the lesser of the two first elements.
-    currentVal = returnLesser(list1[0], list2[0])
-    list1Index = 0  # index - pointer for list 1
-    list2Index = 0  # index - poiner for list 2
+def is_rotation(list1, list2):
+    # lists must be equal in length in order to be a rotation.
+    if len(list1) != len(list2):
+        return False
 
-    # Stop the search when * both * of the index - pointer variables
-    # hit the end of their list length.
-    while list1Index < len(list1) and list2Index < len(list2):
+    # Set two list pointers
+    ptr1 = 0
+    ptr2 = 0
 
-        # Search using the lesser of the two current values
-        currentVal = returnLesser(list1[list1Index], list2[list2Index])
+    startingPointFound = False
+    # Sweep through list2, looking for the starting point of list1
+    while ptr2 < len(list2):
+        if list1[ptr1] == list2[ptr2]:
+            startingPointFound = True
+            break
+        ptr2 += 1
 
-        # Determine if this is a value in common
-        if list1[list1Index] == list2[list2Index]:
-            result.append(currentVal)
+    # If no starting point is found, return False
+    if startingPointFound == False:
+        return False
 
-        # Each time a list DOES contain the current value we are looking for,
-        # advance the list's corresponding index-pointer variable.
-        if list1[list1Index] == currentVal:
-            list1Index += 1
-        if list2[list2Index] == currentVal:
-            list2Index += 1
+    # Sweep through list 1 and 2, verifying that each element matches.
+    # If a mismatch is found, return False.
+    while ptr1 < len(list1):
+        if list1[ptr1] != list2[ptr2]:
+            return False
 
-    return result
+        ptr1 += 1
 
-list_a1 = [1, 3, 4, 6, 7, 9]
-list_a2 = [1, 2, 4, 5, 9, 10]
-# common_elements(list_a1, list_a2) should return [1, 4, 9] (a list).
-print(common_elements(list_a1, list_a2))
+        # list2's pointer can wrap around back to the beginning.
+        if ptr2 < (len(list2) - 1):
+            ptr2 += 1
+        else:
+            ptr2 = 0
 
-list_b1 = [1, 2, 9, 10, 11, 12]
-list_b2 = [0, 1, 2, 3, 4, 5, 8, 9, 10, 12, 14, 15]
-# common_elements(list_b1, list_b2) should return [1, 2, 9, 10, 12] (a list).
-print(common_elements(list_b1, list_b2))
+    return True
 
-list_c1 = [0, 1, 2, 3, 4, 5]
-list_c2 = [6, 7, 8, 9, 10, 11]
-# common_elements(list_c1, list_c2) should return [] (an empty list).
-print(common_elements(list_c1, list_c2))
+# Tests
+list1 = [1, 2, 3, 4, 5, 6, 7]
+list2a = [4, 5, 6, 7, 8, 1, 2, 3]
+list2b = [4, 5, 6, 7, 1, 2, 3]
+list2c = [4, 5, 6, 9, 1, 2, 3]
+list2d = [4, 6, 5, 7, 1, 2, 3]
+list2e = [4, 5, 6, 7, 0, 2, 3]
+list2f = [1, 2, 3, 4, 5, 6, 7]
+
+# is_rotation(list1, list2a) should return False.
+print(is_rotation(list1, list2a) == False)
+# is_rotation(list1, list2b) should return True.
+print(is_rotation(list1, list2b) == True)
+# is_rotation(list1, list2c) should return False.
+print(is_rotation(list1, list2c) == False)
+# is_rotation(list1, list2d) should return False.
+print(is_rotation(list1, list2d) == False)
+# is_rotation(list1, list2e) should return False.
+print(is_rotation(list1, list2e) == False)
+# is_rotation(list1, list2f) should return True.
+print(is_rotation(list1, list2f) == True)
