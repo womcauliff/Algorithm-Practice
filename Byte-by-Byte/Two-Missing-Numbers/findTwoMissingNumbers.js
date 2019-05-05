59ade0681e3c67b187b2e533fa9f003a9b3f8ecb
Will McAuliff <womcauliff@gmail.com>
2019-05-05 18:53:32 -0400

    In-place O(n) solution, handles duplicates

--- Byte-by-Byte/Two-Missing-Numbers/findTwoMissingNumbers.js
+++ Byte-by-Byte/Two-Missing-Numbers/findTwoMissingNumbers.js
@@ -1,9 +1,10 @@
 /**
  * Finds the two missing positive integers from a given series.
+ * Note: Cannot handle duplicate elements.
  * @param {Number[]} arr The array of integers.
  * @returns {Number[]} The two smallest positive elements missing from array.
  */
-function missing(arr) {
+export function missingTwoNoDuplicates(arr) {
   if (!arr) return [1, 2];
 
   // Reduce array to find the total sum of elements in the array (actual),
@@ -50,6 +51,74 @@ function missing(arr) {
   // Else, the series is missing two elements, which when added together,
   // equal the diff.
   return [expected2 - actual2, diff - (expected2 - actual2)];
+}
+
+/**
+ * Finds a given number of missing positive integers from a given series. Can handle
+ * duplicate values and negative integers.
+ * @param {Number[]} arr The array of integers.
+ * @param {Number} numMissing The number of positive integers missing from the given series.
+ * @returns {Number[]} The given number of smallest positive elements missing from array, in ascending order.
+ */
+function missing(arr, numMissing) {
+  // Handle case where no array is passed.
+  if (!arr) {
+    const result = new Array(numMissing);
+    for (let i = 0; i < result.length; i++) {
+      result[i] = i + 1;
+    }
+    return result;
+  }
+
+  // Iterate over list once.
+  for (let i = 0; i < arr.length; i++) {
+    // Begin the swapping-loop if the current value is not in its proper location.
+    // Ignore negative values, and positive values greater than the array length.
+    // (This nested while-loop is actually *not* O(n^2) because each element is only swapped *once*!)
+    while (arr[i] !== i + 1 && arr[i] > 0 && arr[i] <= arr.length) {
+      // Swap current value into its proper destination location.
+      const destinationIndex = arr[i] - 1;
+      const temp = arr[destinationIndex];
+      arr[destinationIndex] = arr[i];
+      arr[i] = temp;
+
+      // Stop swapping once we replace two equivalent values
+      if (arr[i] === arr[destinationIndex]) {
+        break;
+      }
+    }
+  }
+
+  // Iterate over the swapped-elements arary, and record which elements are missing.
+  // Continue until we have recorded the given number missing elements.
+  const missing = [];
+  let currentVal = 1;
+  while (missing.length < numMissing) {
+    let searchIndex = currentVal - 1;
+
+    if (searchIndex >= arr.length) {
+      // If we have swept through list and still need additional missing elements,
+      // check only last element in the list.
+      searchIndex = arr.length - 1;
+    }
+    if (currentVal !== arr[searchIndex]) {
+      // If there is a mismach, this is a missing value from the series.
+      missing.push(currentVal);
+    }
+    currentVal += 1;
+  }
+
+  return missing;
+}
+
+/**
+ * Finds the two missing positive integers from a given series.
+ * @param {Number[]} arr The array of integers.
+ * @returns {Number[]} The two smallest positive elements missing from array, in ascending order.
+ */
+export function missingTwo(arr) {
+  // partial function application
+  return missing(arr, 2);
 }
 
 export default missing;
--- Byte-by-Byte/Two-Missing-Numbers/findTwoMissingNumbers.test.js
+++ Byte-by-Byte/Two-Missing-Numbers/findTwoMissingNumbers.test.js
@@ -1,29 +1,38 @@
-import missing from "./findTwoMissingNumbers";
+import { missingTwo } from "./findTwoMissingNumbers";
 
-describe(`missing()`, () => {
+describe(`missingTwo()`, () => {
   const smallSeries = [1, 4];
   const longSeries = [1, 2, 3, 4, 5, 7, 9];
   const unsorted = [4, 1, 9, 8, 7, 6, 3];
   const missingOne = [1, 2, 3, 4, 6];
   const completeSeries = [6, 5, 4, 3, 2, 1];
+  const messySeries = [-2, 5, 3, 1, 3, 3, 2, 100];
+
+  it(`handles empty series`, () => {
+    expect(missingTwo([])).toEqual([1, 2]);
+  });
 
   it(`finds two missing elements with small series`, () => {
-    expect(missing(smallSeries)).toEqual([2, 3]);
+    expect(missingTwo(smallSeries)).toEqual([2, 3]);
   });
 
   it(`finds two missing elements with long sorted series`, () => {
-    expect(missing(longSeries)).toEqual([6, 8]);
+    expect(missingTwo(longSeries)).toEqual([6, 8]);
   });
 
   it(`finds two missing elements with long unsorted series`, () => {
-    expect(missing(unsorted)).toEqual([2, 5]);
+    expect(missingTwo(unsorted)).toEqual([2, 5]);
   });
 
   it(`returns the missing element and one larger integer given a series with only one missing.`, () => {
-    expect(missing(missingOne)).toEqual([5, 7]);
+    expect(missingTwo(missingOne)).toEqual([5, 7]);
   });
 
   it(`returns the next two larger integers given a complete series`, () => {
-    expect(missing(completeSeries)).toEqual([7, 8]);
+    expect(missingTwo(completeSeries)).toEqual([7, 8]);
+  });
+
+  it(`finds two missing elements given a series with negative integers and duplicates.`, () => {
+    expect(missingTwo(messySeries)).toEqual([4, 6]);
   });
 });
