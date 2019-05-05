c90199982255f5f64b43581db8da336a3df6b168
Will McAuliff <womcauliff@gmail.com>
2019-05-05 12:13:55 -0400

    DCP #2: division solution accounts for zeroes

--- Daily-Coding-Problem/2-Product-of-All-Others/productOfAllOthers.js
+++ Daily-Coding-Problem/2-Product-of-All-Others/productOfAllOthers.js
@@ -6,15 +6,29 @@
  * @return {Number[]} The resulting array.
  */
 function productOfAllOthers(arr) {
+  let numZeroes = 0;
   // Reduce the elements of source array into the total product of all elements
-  const totalProduct = arr.reduce(
-    (runningProduct, currentVal) => (runningProduct *= currentVal),
-    1
-  );
+  const totalProduct = arr.reduce((runningProduct, currentVal) => {
+    if (currentVal === 0) {
+      numZeroes += 1;
+      return runningProduct;
+    }
+    return (runningProduct *= currentVal);
+  }, 1);
 
-  // Map over the source array to return a result array where each
-  // resulting element is the totalProduct divided by the original element.
-  return arr.map(num => totalProduct / num);
+  if (numZeroes > 1) {
+    // If input array has more than two zeroes, then all products will be zero.
+    return arr.map(() => 0);
+  } else if (numZeroes === 1) {
+    // If input array has a single zero, then all products will be zero,
+    // except for the index containing the zero.
+    return arr.map(num => (num === 0 ? totalProduct : 0));
+  } else {
+    // If input array contains no zeroes,
+    // Map over the source array to return a result array where each
+    // resulting element is the totalProduct divided by the original element.
+    return arr.map(num => totalProduct / num);
+  }
 }
 
 // Naive brute-force approach
--- Daily-Coding-Problem/2-Product-of-All-Others/productOfAllOthers.test.js
+++ Daily-Coding-Problem/2-Product-of-All-Others/productOfAllOthers.test.js
@@ -7,3 +7,11 @@ test(`[1, 2, 3, 4, 5] returns [120, 60, 40, 30, 24]`, () => {
 test(`[3, 2, 1] returns [2, 3, 6]`, () => {
   expect(productOfAllOthers([3, 2, 1])).toEqual([2, 3, 6]);
 });
+
+test(`finds result when array has one zero`, () => {
+  expect(productOfAllOthers([6, 2, 0, 3])).toEqual([0, 0, 36, 0]);
+});
+
+test(`finds result when array has two zeros`, () => {
+  expect(productOfAllOthers([4, 0, 9, 1, 0])).toEqual([0, 0, 0, 0, 0]);
+});
